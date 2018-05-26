53f732ba7734c4ecf5288e81d1b602bbc6ec70ee
NanaCode <2503605326@qq.com>
2018-05-27 07:55:30 +0800

    update overview

---
+++ 1_Overview/env_tools.py
@@ -0,0 +1,22 @@
+# _*_ coding: utf-8 _*_
+__author__ = 'Nana'
+__date__ = '2018/4/17 22:42'
+
+# python3.7 18年5月发布最终版本
+# pycharm
+
+# path python目录 python子目录scripts目录 记得重启cmd才能生效
+
+# Linux安装python3.7
+# Linux source code
+# 下载 安装依赖 避免出现ssl的问题
+# 软链接
+# 安装过程类似python3.6
+
+# pycharm配置python解释器
+
+# pycharm的structure
+# view-Tool Windows-Structure
+
+# keymaps
+
---
+++ 1_Overview/sources.py
@@ -0,0 +1,8 @@
+# _*_ coding: utf-8 _*_
+__author__ = 'Nana'
+__date__ = '2018/4/17 22:53'
+
+# http://apply.projectsedu.com/
+# 申请加入私人仓库
+
+# 邀请链接：https://github.com/liyaopinner/AdvancePython/invitations
